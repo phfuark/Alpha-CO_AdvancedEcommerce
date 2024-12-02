1b32a38b9fcae08dda31eaa50dcf0e08ffa00dfb
Paulo P <ct67ca@bosch.com>
2024-12-02 11:25:41 -0300

    02.12.2024 11:25

---
+++ E-COMMERCE_VITE-main/html & css/scripts/database.js
@@ -0,0 +1,166 @@
+// Base de dados de produtos
+window.database = [
+    // Produtos exibidos na home
+    {
+        qnt: "3 | 2",
+        id: 1,
+        tituloProduto: "Peças Dry Fit",
+        preco: 193.03,
+        descricao: "As Peças Dry Fit são confeccionadas com um material leve e respirável, elas têm a capacidade de absorver o suor de maneira eficiente.",
+        imagemProduto: {
+            img1: "./imagens/product1.png",
+            img2: "./imagens/product1.png",
+            img3: "./imagens/product1.png",
+            img4: "./imagens/product1.png"
+        },
+        categoriaProduto: "Roupas",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        qnt: 5,
+        id: 2,
+        tituloProduto: "Peças Dry Fit",
+        preco: 193.03,
+        descricao: "As Peças Dry Fit são confeccionadas com um material leve e respirável, elas têm a capacidade de absorver o suor de maneira eficiente.",
+        imagemProduto: {
+            img1: "./imagens/product2.png",
+            img2: "./imagens/product2.png",
+            img3: "./imagens/product2.png",
+            img4: "./imagens/product2.png"
+        },
+        categoriaProduto: "Roupas",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        qnt: 5,
+        id: 3,
+        tituloProduto: "Peças Dry Fit",
+        preco: 193.03,
+        descricao: "As Peças Dry Fit são confeccionadas com um material leve e respirável, elas têm a capacidade de absorver o suor de maneira eficiente.",
+        imagemProduto: {
+            img1: "./imagens/product3.png",
+            img2: "./imagens/product3.png",
+            img3: "./imagens/product3.png",
+            img4: "./imagens/product3.png"
+        },
+        categoriaProduto: "Roupas",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        qnt: 1,
+        id: 4,
+        tituloProduto: "Peças Dry Fit",
+        preco: 193.03,
+        descricao: "As Peças Dry Fit são confeccionadas com um material leve e respirável, elas têm a capacidade de absorver o suor de maneira eficiente.",
+        imagemProduto: {
+            img1: "./imagens/product4.png",
+            img2: "./imagens/product4.png",
+            img3: "./imagens/product4.png",
+            img4: "./imagens/product4.png"
+        },
+        categoriaProduto: "Roupas",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        qnt: 5,
+        id: 5,
+        tituloProduto: "Peças Dry Fit",
+        preco: 193.03,
+        descricao: "As Peças Dry Fit são confeccionadas com um material leve e respirável, elas têm a capacidade de absorver o suor de maneira eficiente.",
+        imagemProduto: {
+            img1: "./imagens/product5.png",
+            img2: "./imagens/product5.png",
+            img3: "./imagens/product5.png",
+            img4: "./imagens/product5.png"
+        },
+        categoriaProduto: "Roupas",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        qnt: "3 | 2",
+        id: 6,
+        tituloProduto: "Peças Dry Fit",
+        preco: 193.03,
+        descricao: "As Peças Dry Fit são confeccionadas com um material leve e respirável, elas têm a capacidade de absorver o suor de maneira eficiente.",
+        imagemProduto: {
+            img1: "./imagens/product1.png",
+            img2: "./imagens/product1.png",
+            img3: "./imagens/product1.png",
+            img4: "./imagens/product1.png"
+        },
+        categoriaProduto: "Roupas",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        qnt: 5,
+        id: 7,
+        tituloProduto: "Peças Dry Fit",
+        preco: 193.03,
+        descricao: "As Peças Dry Fit são confeccionadas com um material leve e respirável, elas têm a capacidade de absorver o suor de maneira eficiente.",
+        imagemProduto: {
+            img1: "./imagens/product2.png",
+            img2: "./imagens/product2.png",
+            img3: "./imagens/product2.png",
+            img4: "./imagens/product2.png"
+        },
+        categoriaProduto: "Roupas",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        qnt: 5,
+        id: 8,
+        tituloProduto: "Peças Dry Fit",
+        preco: 193.03,
+        descricao: "As Peças Dry Fit são confeccionadas com um material leve e respirável, elas têm a capacidade de absorver o suor de maneira eficiente.",
+        imagemProduto: {
+            img1: "./imagens/product3.png",
+            img2: "./imagens/product3.png",
+            img3: "./imagens/product3.png",
+            img4: "./imagens/product3.png"
+        },
+        categoriaProduto: "Roupas",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        qnt: 1,
+        id: 9,
+        tituloProduto: "Peças Dry Fit",
+        preco: 193.03,
+        descricao: "As Peças Dry Fit são confeccionadas com um material leve e respirável, elas têm a capacidade de absorver o suor de maneira eficiente.",
+        imagemProduto: {
+            img1: "./imagens/product4.png",
+            img2: "./imagens/product4.png",
+            img3: "./imagens/product4.png",
+            img4: "./imagens/product4.png"
+        },
+        categoriaProduto: "Roupas",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        qnt: 5,
+        id: 9,
+        tituloProduto: "Peças Dry Fit",
+        preco: 193.03,
+        descricao: "As Peças Dry Fit são confeccionadas com um material leve e respirável, elas têm a capacidade de absorver o suor de maneira eficiente.",
+        imagemProduto: {
+            img1: "./imagens/product5.png",
+            img2: "./imagens/product5.png",
+            img3: "./imagens/product5.png",
+            img4: "./imagens/product5.png"
+        },
+        categoriaProduto: "Roupas",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+];
+
+
